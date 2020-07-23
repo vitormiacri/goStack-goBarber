262a37ffce9d012941f3a85b973d89d5903941e2
Vitor Morelli <vitao.miacri@gmail.com>
2020-07-23 09:51:33 -0300

    [feat] Create a HashProvider

---
+++ backend/src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -0,0 +1,14 @@
+import { hash, compare } from 'bcryptjs';
+import IHashProvider from '../models/IHashProvider';
+
+class BCryptHashProvider implements IHashProvider {
+  public async generatehash(payload: string): Promise<string> {
+    return hash(payload, 8);
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return compare(payload, hashed);
+  }
+}
+
+export default BCryptHashProvider;
